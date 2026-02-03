b6b6f2765a77a4991096e007d2661dd673ce1125
AlexPopCyberSec <aleksandarpopovfx@gmail.com>
2026-02-03 16:52:54 +0700

    Switching to official tailwind integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,13 @@
 import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
 import tailwindcss from '@tailwindcss/vite';
+// DELETE the import tailwindcss line
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://alexpopov.tech',
   integrations: [sitemap()],
+
   vite: {
     plugins: [tailwindcss()],
   },
