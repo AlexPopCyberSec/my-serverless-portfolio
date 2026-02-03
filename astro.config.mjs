4f4fc7271748f61bbf6757f6c9d6d0c0920ed368
AlexPopCyberSec <aleksandarpopovfx@gmail.com>
2026-02-03 15:36:07 +0700

    Fixed build config, added sitemap and robots.txt

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,13 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-import tailwindcss from '@tailwindcss/vite';
 import sitemap from '@astrojs/sitemap';
+import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://alexpopov.tech/', // <--- This line is critical for the sitemap
-  integrations: [sitemap()],      // <--- This activates the sitemap plugin
+  site: 'https://alexpopov.tech',
+  integrations: [sitemap()],
   vite: {
-    plugins: [tailwindcss()]
-  }
+    plugins: [tailwindcss()],
+  },
 });
